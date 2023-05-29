613537aa78fb9e514036ee3cc95149a6387232a8
xi4545c9@163.com <xi4545c9@163.com>
2023-05-29 14:53:25 +0800

    引入antd

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+builder.Services.AddAntDesign();
 
 var app = builder.Build();
 
